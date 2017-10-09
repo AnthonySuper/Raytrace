d8bef0a0c9bb52d08529d78faf49a2f3cfd752b7
Anthony Super <anthony@noided.media>
2017-10-09 12:40:09 -0600

    more comments

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -6,25 +6,40 @@ namespace NM {
     RayIntersection Triangle::checkIntersection(const NM::Ray &ray) {
         Vec4 edgeA = (b - a);
         Vec4 edgeB = (c - a);
+        // P = D x E2
         Vec4 pvec = ray.direction.cross(edgeB);
+        // 1 / (P dot E) is the determinent of the matrix
         FloatType det = edgeA.dot(pvec);
+        // If det is zero then we can't do this calculation, so 
+        // our ray is oblique to the triangle, essentially
         if(det > - Constants::epsilon && det < Constants::epsilon) {
             return {};
         }
+        // Find the inverse det to apply to each member of the vector
         FloatType invDet = (1.0 / det);
+        // This is vector T in the original paper
         Vec4 toVert = ray.position - a;
+        // If this u is less than zero or greater than one, bail early,
+        // as we won't actually intersect the triangle
+        // (this finds just one component of the eventual "solution vector"
+        // so we can bail more quickly, basically)
         FloatType u = (toVert.dot(pvec)) * invDet;
         if(u < 0.0 || u > 1.0) {
             return {};
         }
+        // Q in the original paper
         Vec4 q = toVert.cross(edgeA);
+        // This is the v in baycentric coordinates
         FloatType v = invDet * ray.direction.dot(q);
+        // One again, bail early if we're able
         if(v < 0.0 || (u + v) > 1.0) {
             return {};
         }
+        // Finally find our T parameter 
         FloatType t = invDet * edgeB.dot(q);
-        // This means that the triangle is on the same point as the ray
-        // Due to how floats work, this can be close to zero but not zero.
+        // If t = 0 we have the fun case where the ray origin is *exactly*
+        // along the triangle. In this case, we consider no intersection to 
+        // have taken place.
         if(t > Constants::epsilon) {
             return {
                 ray.position + (ray.direction * t),
@@ -32,7 +47,8 @@ namespace NM {
                 ray
             };
         }
-        // There's an intersection but it's behind us
+        // There's an "intersection" but it's behind us or on our origin.
+        // Either way, we don't care about it for purposes of raytracing.
         return {};
     }
     
